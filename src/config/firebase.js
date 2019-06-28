9806199684ca8630b5eb8101eea99f1a885792fb
Usama Baig <usamabaig776@live.com>
2019-06-29 00:22:30 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,7 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA7zpamQRXJRr_G-l9jJW5FLO0v6ks0xM8",
-    authDomain: "react-food-delivery-app.firebaseapp.com",
-    databaseURL: "https://react-food-delivery-app.firebaseio.com",
-    projectId: "react-food-delivery-app",
-    storageBucket: "react-food-delivery-app.appspot.com",
-    messagingSenderId: "1023662019698",
-    appId: "1:1023662019698:web:f96a90c3891f53cf"
+
 };
 
 firebase.initializeApp(firebaseConfig);
